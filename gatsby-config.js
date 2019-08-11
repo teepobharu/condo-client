2204ced42e204a62677c64bb257066276e3ef620
teepobH <teepob.haru@gmail.com>
2019-08-11 17:04:06 +0700

    update url config

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,9 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `http://localhost:1337`,
+        apiURL: process.env.DEPLOY_URL
+          ? "https://condocms.herokuapp.com"
+          : "http://localhost:1337",
         prodApiURL: `https://condocms.herokuapp.com`,
         contentTypes: [
           `condo`,
